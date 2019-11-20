1adc86f5383393eb41b40b23ee213244ec1d3c40
Esther Gassner <egassner1030@gmail.com>
2019-11-19 22:09:45 -0500

    built the outer walls of the pinball board

--- src/main/java/pinball/PinballComponent.java
+++ src/main/java/pinball/PinballComponent.java
@@ -1,16 +1,66 @@
 package pinball;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 
 import javax.swing.JComponent;
+import java.awt.*;
 
 public class PinballComponent extends JComponent {
 
-    private final World world;
+    private static final float WIDTH = 900;
+    private static final float HEIGHT = 1300;
 
-    public PinballComponent() {
+    private static final float BOX_TO_SCREEN = 10f;
+    private static final float SCREEN_TO_BOX = 1f / BOX_TO_SCREEN;
+
+    private final World world;
+    private final Body bottom, right, left, top;
+
+    public PinballComponent()
+    {
         world = new World(new Vector2(0, 9.8f), false);
+        bottom = createWall(100f * SCREEN_TO_BOX, (HEIGHT + 100) * SCREEN_TO_BOX, WIDTH * SCREEN_TO_BOX, 1f * SCREEN_TO_BOX);
+        top = createWall(100f * SCREEN_TO_BOX, 100f * SCREEN_TO_BOX, WIDTH * SCREEN_TO_BOX, 1f * SCREEN_TO_BOX);
+        right = createWall(100f * SCREEN_TO_BOX, 100f * SCREEN_TO_BOX, 1f * SCREEN_TO_BOX, HEIGHT * SCREEN_TO_BOX);
+        left = createWall((WIDTH + 100) * SCREEN_TO_BOX, 100f * SCREEN_TO_BOX, 1f * SCREEN_TO_BOX, HEIGHT * SCREEN_TO_BOX) ;
     }
 
+    private Body createWall(float vX, float vY, float hX, float hY)
+    {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(new Vector2(vX, vY));
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        Body wall = world.createBody(bodyDef);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(hX, hY);
+        fixtureDef.shape = shape;
+        fixtureDef.restitution = 1;
+        wall.createFixture(fixtureDef);
+        return wall;
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphics)
+    {
+        super.paintComponent(graphics);
+
+        graphics.fillRect((int)(bottom.getPosition().x * BOX_TO_SCREEN),
+                (int)(bottom.getPosition().y * BOX_TO_SCREEN),
+                (int)WIDTH, 1);
+
+        graphics.fillRect((int)(top.getPosition().x * BOX_TO_SCREEN),
+                (int)(top.getPosition().y * BOX_TO_SCREEN),
+                (int)WIDTH, 1);
+
+        graphics.fillRect((int)(right.getPosition().x * BOX_TO_SCREEN),
+                (int)(right.getPosition().y * BOX_TO_SCREEN),
+                1, (int)HEIGHT);
+
+        graphics.fillRect((int)(left.getPosition().x * BOX_TO_SCREEN),
+                (int)(left.getPosition().y * BOX_TO_SCREEN),
+                1, (int)HEIGHT);
+    }
 }
--- src/main/java/pinball/PinballFrame.java
+++ src/main/java/pinball/PinballFrame.java
@@ -1,8 +1,18 @@
 package pinball;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class PinballFrame extends JFrame {
+
+    private PinballFrame()
+    {
+        setSize(1200, 1600);
+        setTitle("Pinball");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        add(new PinballComponent(), BorderLayout.CENTER);
+    }
 
     public static void main(String[] args) {
         new PinballFrame().setVisible(true);
