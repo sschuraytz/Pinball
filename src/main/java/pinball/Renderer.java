0a0d807d480bd06baf22ceb95766b01455a808a2
Ayelet <ayeletcb@gmail.com>
2019-11-21 18:35:16 -0500

    added renderer

---
+++ src/main/java/pinball/Renderer.java
@@ -0,0 +1,75 @@
+package pinball;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
+import java.awt.*;
+
+public class Renderer {
+
+    private World world;
+    private final float BOX2D_TO_SCREEN;
+
+    public Renderer(World world, float box2DToScreenRatio) {
+        this.world = world;
+        BOX2D_TO_SCREEN = box2DToScreenRatio;
+    }
+
+    public void render(Graphics2D graphics2D) {
+        Array<Body> bodies = new Array<Body>();
+        world.getBodies(bodies);
+        for (Body body : bodies) {
+            render(graphics2D, body);
+        }
+    }
+
+    private void render(Graphics2D graphics2D, Body body) {
+        Vector2 position = body.getPosition();
+        Shape shape = body.getFixtureList().get(0).getShape();
+        Shape.Type type = shape.getType();
+        float angle = body.getAngle();
+        switch (type) {
+            case Circle:
+                renderCircle(graphics2D, position, (int) shape.getRadius());
+                break;
+            case Polygon:
+                renderPolygon(graphics2D, (PolygonShape) shape, position, angle);
+                break;
+        }
+    }
+
+    private void renderCircle(Graphics2D graphics2D, Vector2 position, int radius) {
+        int screenRadius = Math.round(radius * BOX2D_TO_SCREEN);
+        int diameter = screenRadius * 2;
+        graphics2D.fillOval(Math.round(position.x * BOX2D_TO_SCREEN) - screenRadius,
+                Math.round(position.y * BOX2D_TO_SCREEN) - screenRadius,
+                diameter,
+                diameter);
+    }
+
+    private void renderPolygon(Graphics2D graphics2D, PolygonShape polygon, Vector2 position, float angle) {
+        int vertices = polygon.getVertexCount();
+        if (vertices > 0) {
+            int[] xCoordinates = new int[vertices];
+            int[] yCoordinates = new int[vertices];
+            Vector2 vector = new Vector2();
+            int startingX = Math.round(position.x * BOX2D_TO_SCREEN);
+            int startingY = Math.round(position.y * BOX2D_TO_SCREEN);
+            xCoordinates[0] = startingX;
+            yCoordinates[0] = startingY;
+
+            for (int vertex = 1; vertex < vertices; vertex++) {
+                polygon.getVertex(vertex, vector);
+                int vectorLength = Math.round(vector.x * BOX2D_TO_SCREEN);
+                int vectorHeight = Math.round(vector.y * BOX2D_TO_SCREEN);
+                xCoordinates[vertex] = xCoordinates[vertex - 1] + vectorLength;
+                yCoordinates[vertex] = yCoordinates[vertex - 1] + vectorHeight;
+            }
+            graphics2D.drawPolygon(xCoordinates, yCoordinates, vertices);
+        }
+    }
+}
