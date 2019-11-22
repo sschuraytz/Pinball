656b998d41697038959b98111d87668bd7c852dd
HelenSutton <38437834+HelenSutton@users.noreply.github.com>
2019-11-21 20:06:40 -0500

    put keylisteners into seperate class and added a controller class

--- src/main/java/PinballFrame.java
+++ src/main/java/PinballFrame.java
@@ -3,7 +3,9 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class PinballFrame extends JFrame  implements KeyListener {
+public class PinballFrame extends JFrame {
+    PinballKeyListeners pinballKeyListeners;
+    PinballController pinballController = new PinballController();
 
     public PinballFrame (){
 
@@ -11,60 +13,17 @@ public class PinballFrame extends JFrame  implements KeyListener {
         setSize(600,800);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        this.addKeyListener(this);
+
+        pinballKeyListeners = new PinballKeyListeners(pinballController);
+        this.addKeyListener(pinballKeyListeners);
         JPanel root = new JPanel();
         PinballComponent pinballComponent = new PinballComponent();
         root.setLayout(new BorderLayout());
         root.add(pinballComponent, BorderLayout.CENTER);
         setContentPane(root);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
 
     }
 
 
-    private void releaseball() {
-        System.out.println("release ball");
-
     }
 
-    private void leftFlipper() {
-        System.out.println("left flipper");
-    }
-
-    private void rightFlipper() {
-        System.out.println("right flipper");
-
-    }
-    private void ballPressure() {
-        System.out.println("ball pressed");
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_RIGHT){
-            rightFlipper();
-        }
-        else if (keyCode == KeyEvent.VK_LEFT){
-            leftFlipper();
-        }
-        else if (keyCode == KeyEvent.VK_UP){
-            ballPressure();
-        }
-
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_UP){
-            releaseball();
-        }
-
-    }
-}
