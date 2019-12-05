a4260a517bc73b9b7604dfe4e7e45a12458d7550
sschuraytz <sbschuraytz@gmail.com>
2019-12-05 18:56:52 -0500

    removed original test that did not sufficiently utilize mocking

--- src/test/java/test/schuraytz/RendererTest.java
+++ src/test/java/test/schuraytz/RendererTest.java
@@ -52,39 +52,4 @@ public class RendererTest {
         verify(graphics2D).fillOval(10, 10, 20, 20);
 
     }
-
-    @Test
-    public void render_circle() {
-
-        //given
-        float BOX_TO_SCREEN = 10f;
-        float SCREEN_TO_BOX = 1/BOX_TO_SCREEN;
-        int radius = 10;
-        World world = new World(new Vector2(0, 9.8f), false);
-        Renderer renderer = new Renderer(world, BOX_TO_SCREEN);
-
-        BodyDef bodyDef = new BodyDef();
-        FixtureDef fixtureDef = new FixtureDef();
-
-        CircleShape circle = new CircleShape();
-        circle.setPosition(new Vector2(radius, radius));
-        circle.setRadius(radius * SCREEN_TO_BOX);
-        fixtureDef.shape = circle;
-
-        Body circleBody = world.createBody(bodyDef);
-        circleBody.createFixture(fixtureDef);
-
-        //https://stackoverflow.com/questions/21069911/junit-testcase-for-a-method-with-graphics-parameter
-        Graphics2D gMock = mock(Graphics2D.class);
-
-        //when
-        renderer.render(gMock);
-
-        //then
-        Mockito.verify(gMock).fillOval(Math.round(circleBody.getPosition().x * BOX_TO_SCREEN - radius),
-                Math.round(circleBody.getPosition().y * BOX_TO_SCREEN - radius),
-                radius * 2, radius * 2
-                );
-    }
-
 }
