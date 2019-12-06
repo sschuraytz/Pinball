ee5a993ce3068dc149a327957b029322ff1723d4
Kaila Minkowitz <kailamink@gmail.com>
2019-12-05 20:24:17 -0500

    had two pinball frames. deleted one

--- src/main/java/pinball/PinballFrame.java
+++ src/main/java/pinball/PinballFrame.java
@@ -20,15 +20,6 @@ public class PinballFrame extends JFrame {
         add(new PinballComponent(), BorderLayout.CENTER);
     }
 
-    private PinballFrame()
-    {
-        setSize(1200, 1750);
-        setTitle("Pinball");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        add(new PinballComponent(), BorderLayout.CENTER);
-    }
-
     public static void main(String[] args) {
         new PinballFrame().setVisible(true);
     }
